632d01378b4132f3b8f75b4d7cb10b3461b7c240
OmarEleno <tu@email.com>
2026-04-02 11:27:32 -0700

    commit de prueba

---
+++ src/main/java/com/omar/demo/dao/TutoradoService.java
@@ -0,0 +1,9 @@
+package com.omar.demo.dao;
+
+import com.omar.demo.domain.Tutorado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TutoradoService extends JpaRepository<Tutorado, Integer> {
+
+
+}
