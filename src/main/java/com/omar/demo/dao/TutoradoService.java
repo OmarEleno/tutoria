65feb0a1f8657753944e29790262dacf7721fb5e
omareleno <perezcervantesomareleno@gmail.com>
2026-04-06 18:13:09 -0700

    hice uno de prueba

--- src/main/java/com/omar/demo/dao/TutoradoService.java
+++
@@ -1,9 +0,0 @@
-package com.omar.demo.dao;
-
-import com.omar.demo.domain.Tutorado;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TutoradoService extends JpaRepository<Tutorado, Integer> {
-
-
-}
