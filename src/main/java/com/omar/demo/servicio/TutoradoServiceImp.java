e972bd7ddd7ac2dcd6f253bed069f130568cc0f3
omareleno <perezcervantesomareleno@gmail.com>
2026-04-08 16:38:34 -0700

    un service

---
+++ src/main/java/com/omar/demo/servicio/TutoradoServiceImp.java
@@ -0,0 +1,40 @@
+package com.omar.demo.servicio;
+
+import com.omar.demo.dao.TutoradoRepository;
+import com.omar.demo.domain.Carrera;
+import com.omar.demo.domain.Tutorado;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class TutoradoServiceImp implements TutoradoService {
+
+    @Autowired
+    TutoradoRepository tutoradoRepository;
+
+    @Override
+    public Tutorado localizarPorId(Integer id) {
+        return tutoradoRepository.findById(id).orElseThrow(null);
+    }
+
+    @Override
+    public List<Tutorado> localizarPorNombre(String nombre) {
+        return tutoradoRepository.findByNombre(nombre);
+    }
+
+    @Override
+    public List<Tutorado> localizarPorApellidoPaterno(String apellidoPaterno) {
+        return tutoradoRepository.findByApellidoPaterno(apellidoPaterno);
+    }
+
+    @Override
+    public List<Tutorado> localizarPorApellidoMaterno(String apellidoMaterno) {
+        return tutoradoRepository.findByApellidoMaterno(apellidoMaterno);
+    }
+
+    @Override
+    public List<Tutorado> localizarPorCarrera(Carrera carrera) {
+        return tutoradoRepository.findByCarrera(carrera);
+    }
+}
